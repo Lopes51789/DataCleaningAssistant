0c5cc1b58fa9c6bcc556a586c756000b36037b55
Lopes51789 <gablopesc10@gmail.com>
2025-02-03 10:01:29 -0300

    update cleanData.py

--- cleanData.py
+++ cleanData.py
@@ -6,11 +6,29 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression, LinearRegression
-import dataclasses
 
 
 class DataFrame:
     def __init__(self, filepath: str = None, df: pd.DataFrame = None):
+        """
+        __init__ constructor for DataFrame class.
+
+        Parameters
+        ----------
+        filepath : str, optional
+            The path to the file containing the data. The default is None.
+        df : pd.DataFrame, optional
+            The DataFrame containing the data. The default is None.
+
+        Raises
+        ------
+        ValueError
+            If both filepath and df are provided or neither is provided.
+
+        Returns
+        -------
+        None
+        """
         if df is not None and filepath is None:
             self.df = df
         
@@ -33,25 +51,69 @@ class DataFrame:
             raise ValueError("Either filepath or df must be provided, not both")
 
     def get_duplicates(self):
+        """
+        Returns a boolean Series denoting duplicate rows.
+
+        Returns
+        -------
+        Series of bool
+            For each element in the DataFrame, True if the row is a duplicate and
+            False otherwise.
+        """
         return self.df.duplicated()
 
     def get_amount_duplicates(self):
+        """
+        Returns the number of duplicate rows in the DataFrame.
+
+        Returns
+        -------
+        int
+            The number of duplicate rows in the DataFrame.
+        """
         return self.df.duplicated().sum()
     
-    def get_which_missing_values(self):
+    def get_missing_value(self):
+        """
+        Returns a Series with counts of missing values in each column.
+
+        Returns
+        -------
+        Series
+            A Series with the count of missing values in each column.
+        """
         return self.df.isnull().sum()
     
     def get_amount_missing_values(self):
+        """
+        Returns the total number of missing values in the DataFrame.
+
+        Returns
+        -------
+        int
+            The total count of missing values across all columns in the DataFrame.
+        """
         return self.df.isnull().sum().sum()
     
-    def get_stats(self):
-        return self.df.describe()
+    def get_columns_missing_values(self):
+        """
+        Returns a list of column names that contain missing values.
+
+        Returns
+        -------
+        list
+            A list of column names that contain missing values.
+        """
+        return list(self.df.columns[self.df.isnull().any()])
+    
+    def get_general_stats(self):
+        return self.df.describe(include='all')
     
     def get_info(self):
         return self.df.info()
     
     def get_data_report(self):
-        print(self.get_info())
+        return self.get_info(), self.get_stats(), self.get_missing_value(), self.get_amount_missing_values(), self.get_duplicates(), self.get_amount_duplicates()
 
     def get_correlation(self):
         return self.df.corr()
@@ -61,17 +123,24 @@ class DataFrame:
     
     def categorical_to_numeric(self):
         labelencoder = LabelEncoder()
+        onehotencoder = OneHotEncoder()
+
         for col in self.df.columns:
             if self.df[col].dtype == 'object':
                 self.df[col] = labelencoder.fit_transform(self.df[col])
+            elif self.df[col].dtype == 'bool':
+                self.df[col] = onehotencoder.fit_transform(self.df[col])
 
-        return labelencoder
+        return [labelencoder, onehotencoder]
     
-    def fill_missing_values(self):
+    def fill_column_missing_values(self, column, method="mean"):
+        if method == "mean":
+            self.df[column] = self.df[column].fillna(self.df[column].mean())
+        elif method == "median":
+            self.df[column] = self.df[column].fillna(self.df[column].median())
+        elif method == "delete":
+            self.df[column] = self.df[column].dropna()
         
-        self.df.fillna(method="ffill", inplace=True)
-        self.df.fillna(method="bfill", inplace=True)
-        return self
     
 
 
@@ -79,7 +148,11 @@ def test():
     filepath2 = "testcsv\\tb_lobby_stats_player.csv"
     df2 = DataFrame(filepath2)
     df2.categorical_to_numeric()
-    print(df2.get_correlation())
+    for col in df2.get_columns_missing_values():
+        df2.fill_column_missing_values(col, method="median")
+    print(df2.get_general_stats())
+
+    
 
 
 
